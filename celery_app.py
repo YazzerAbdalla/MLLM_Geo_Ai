ad7c9b756f2dbdbcfb426312d852a30e76c660ec
youssef zanoun <youssefzanoun51@gmail.com>
2026-06-13 05:41:45 +0300

    celery change AI17

--- celery_app.py
+++ celery_app.py
@@ -12,7 +12,7 @@ celery_app = Celery(
     "mllm_geo_ai",
     broker=REDIS_URL,
     backend=REDIS_URL,
-    include=["tasks.load_area", "tasks.classify", "tasks.train_mllm"]
+    include=["tasks.load_area", "tasks.classify","tasks.train_mllm"]
 )
 
 celery_app.conf.update(
@@ -23,7 +23,7 @@ celery_app.conf.update(
     task_routes={
         "tasks.classify.*": {"queue": "gpu"},
         "tasks.load_area.*": {"queue": "cpu"},
-        "tasks.train_mllm.*": {"queue": "cpu"},
+        "tasks.train_mllm.*": {"queue": "gpu"}
     }
 )
 
